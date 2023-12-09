e5edbec067873f8ccb148c78256382c509a74467
zined <zinedinarnaut085@gmail.com>
2023-12-09 14:20:18 +1100

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/', // or the desired output path
+          },
+        },
+      },
+      // Add other loaders for different file types if needed
+    ],
+  },
   images: {
     remotePatterns: [
       {
